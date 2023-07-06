0374182e9fcc3597a7c3883fe5c1276394368295
Roberto Araripe <roberto.araripe@outlook.com>
2023-07-06 10:52:50 -0300

    Criação do Projeto

---
+++ src/main/java/com/robertoararipe/project/Projetospring1Application.java
@@ -0,0 +1,13 @@
+package com.robertoararipe.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetospring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetospring1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/robertoararipe/project/Projetospring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertoararipe.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetospring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
