04f0c9531f72d502572f902408c78b4eb7c49882
Roberto Araripe <roberto.araripe@outlook.com>
2023-07-07 11:17:38 -0300

    Configuração do banco de dados H2 e criação automática da base de dados.

--- src/main/java/com/robertoararipe/project/domain/Categoria.java
+++ src/main/java/com/robertoararipe/project/domain/Categoria.java
@@ -1,10 +1,17 @@
 package com.robertoararipe.project.domain;
 
 import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	
