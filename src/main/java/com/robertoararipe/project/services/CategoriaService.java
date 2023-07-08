4a90298ecf085a3b135622e7705b9ee2e697c996
Roberto Araripe <roberto.araripe@outlook.com>
2023-07-08 11:57:59 -0300

    Criação do Repository e Service para Categoria

---
+++ src/main/java/com/robertoararipe/project/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.robertoararipe.project.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.robertoararipe.project.domain.Categoria;
+import com.robertoararipe.project.repositories.CategoriaRepository;
+
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
