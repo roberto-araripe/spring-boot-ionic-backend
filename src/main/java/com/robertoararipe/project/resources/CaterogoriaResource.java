8c7637a75d0d49fa02aa7174c779d0a3575a8a78
Roberto Araripe <roberto.araripe@outlook.com>
2023-07-10 12:02:19 -0300

    Criação de operação de instanciação

--- src/main/java/com/robertoararipe/project/resources/CaterogoriaResource.java
+++ src/main/java/com/robertoararipe/project/resources/CaterogoriaResource.java
@@ -23,7 +23,7 @@ public class CaterogoriaResource {
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(obj); 
 				
 	}
 
