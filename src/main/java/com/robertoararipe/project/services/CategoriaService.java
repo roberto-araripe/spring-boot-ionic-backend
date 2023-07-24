65338ce1f169ce1ae624ee890a13e7c4cbc995b6
Roberto Araripe <roberto.araripe@outlook.com>
2023-07-24 14:01:13 -0300

    Ajustes no Endpoint /categorias/{id}

--- src/main/java/com/robertoararipe/project/services/CategoriaService.java
+++ src/main/java/com/robertoararipe/project/services/CategoriaService.java
@@ -1,14 +1,12 @@
 package com.robertoararipe.project.services;
 
 import java.util.Optional;
-
-import org.hibernate.ObjectNotFoundException;
+import com.robertoararipe.project.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.robertoararipe.project.domain.Categoria;
 import com.robertoararipe.project.repositories.CategoriaRepository;
-
 
 @Service
 public class CategoriaService {
@@ -18,6 +16,7 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
