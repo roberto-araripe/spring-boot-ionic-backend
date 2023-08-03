39fee8928d6e2f389cafe8971a8952ec7c5f4591
Roberto Araripe <roberto.araripe@outlook.com>
2023-08-03 17:51:19 -0300

    Paginação com parâmetros de requisições opcionais

--- src/main/java/com/robertoararipe/project/services/CategoriaService.java
+++ src/main/java/com/robertoararipe/project/services/CategoriaService.java
@@ -3,14 +3,17 @@ package com.robertoararipe.project.services;
 import java.util.List;
 import java.util.Optional;
 
-import com.robertoararipe.project.services.exceptions.DataIntegrityException;
-import com.robertoararipe.project.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.robertoararipe.project.domain.Categoria;
 import com.robertoararipe.project.repositories.CategoriaRepository;
+import com.robertoararipe.project.services.exceptions.DataIntegrityException;
+import com.robertoararipe.project.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -46,5 +49,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
  }
